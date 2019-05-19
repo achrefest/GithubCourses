0d9aee37077d87797be9ba67e06f7bfb7ea4acf6
achrefest <39643293+achrefest@users.noreply.github.com>
2019-05-19 17:44:09 +0100

    ajouter les scripts linux précédemment crées

---
+++ modifmeminfo.sh
@@ -0,0 +1,25 @@
+rm -f test.txt
+rm -f testt.txt
+#rm -f testtt.txt
+touch test.txt
+touch testt.txt
+#touch testtt.txt
+cat /proc/meminfo >> test.txt  
+grep -E "(MemTotal)|(MemFree)|(MemAvailable)|(Cached)" test.txt >> testt.txt
+WordCount=$(wc -l testt.txt | tr -dc '0-9')
+#echo $WordCount
+#echo $WordCount
+line=""
+line2=""
+#echo $WordCount
+t=8
+for ((i=1;i<=$WordCount;i++));do
+	#echo 1
+	#echo "$WordCount"; 
+	line=$(head -$i testt.txt | tail -1)
+	line=$line"</br>"
+	echo $line
+#;line1=`"$line" `;echo "$line2"; 
+done
+rm -f test.txt
+rm -f testt.txt
